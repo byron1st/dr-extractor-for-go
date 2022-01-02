a0ce57cbdd525cbae04ce36439864be50025884c
Hwi Ahn <byron1st@icloud.com>
2022-01-02 13:51:30 +0900

    Fix package path

--- lib/extractor.go
+++ lib/extractor.go
@@ -2,6 +2,7 @@ package lib
 
 import (
 	"fmt"
+	"strings"
 
 	"golang.org/x/tools/go/callgraph"
 	"golang.org/x/tools/go/callgraph/static"
@@ -11,7 +12,15 @@ import (
 
 func ExtractCallgraph(pkgName string) error {
 	cfg := &packages.Config{
-		Mode:  packages.LoadAllSyntax,
+		Mode: packages.NeedDeps |
+			packages.NeedSyntax |
+			packages.NeedTypesInfo |
+			packages.NeedTypes |
+			packages.NeedTypesSizes |
+			packages.NeedImports |
+			packages.NeedName |
+			packages.NeedFiles |
+			packages.NeedCompiledGoFiles,
 		Tests: false,
 	}
 	initial, err := packages.Load(cfg, pkgName)
@@ -29,11 +38,19 @@ func ExtractCallgraph(pkgName string) error {
 	cg.DeleteSyntheticNodes()
 
 	if err := callgraph.GraphVisitEdges(cg, func(edge *callgraph.Edge) error {
+		caller := edge.Caller.Func.Pkg.Pkg.Path()
+		if !strings.Contains(caller, pkgName) {
+			return nil
+		}
+
 		pos := prog.Fset.Position(edge.Pos())
-		caller := edge.Caller.String()
 		filename := pos.Filename
+		if filename == "" {
+			return nil
+		}
+
 		line := pos.Line
-		callee := edge.Callee.String()
+		callee := fmt.Sprintf("%s.%s", edge.Callee.Func.Pkg.Pkg.Path(), edge.Callee.Func.Name())
 
 		relation := Relation{
 			Source:   removeCharacters(caller, "(", ")", "*"),
